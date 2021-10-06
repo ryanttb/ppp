564dd765123cf7d689aada609107e3b2583c032b
ryanttb <ryan@trippingthebits.com>
2021-10-05 21:31:26 -0400

    more ch06 calc

--- c06/calc.cpp
+++ c06/calc.cpp
@@ -10,6 +10,10 @@ const char TOKEN_KIND_PLUS = '+';
 const char TOKEN_KIND_MINUS = '-';
 const char TOKEN_KIND_MUL = '*';
 const char TOKEN_KIND_DIV = '/';
+
+const char TOKEN_KIND_QUIT = 'q';
+const char TOKEN_KIND_EVAL = ';';
+const char TOKEN_KIND_UNSET = '?';
 const char TOKEN_KIND_ERROR = 'E';
 
 const double INVALID_VALUE = DBL_MIN;
@@ -19,6 +23,7 @@ public:
     char kind;
     double value;
 
+    Token() : kind(TOKEN_KIND_UNSET) {}
     Token(char k) : kind(k), value(0.0) {}
     Token(double v) : kind(TOKEN_KIND_NUMBER), value(v) {}
     Token(char k, double v) : kind(k), value(v) {}
@@ -30,37 +35,55 @@ public:
     }
 };
 
+class TokenStream {
+public:
+    Token get() {
+        Token t;
 
-Token get_token()
-{
-    double d;
-    cin >> d;
+        if (m_next_token.kind != TOKEN_KIND_UNSET) {
+            t = m_next_token;
+            m_next_token = Token();
+        } else {
+            // bug: eats + and - signs
+            double d;
+            cin >> d;
 
-    //cout << "d: " << d << endl;
+            //cout << "d: " << d << endl;
 
-    if (cin) {
-        Token dt = Token(d);
-        //cout << dt.str() << endl;
-        return dt;
+            if (cin) {
+                t = Token(d);
+                //cout << t.str() << endl;
+            } else {
+                char c;
+                cin >> c;
+
+                //cout << "c: " << c << endl;
+
+                if (cin) {
+                    t = Token(c);
+                    //cout << t.str() << endl;
+                } else {
+                    t = Token(TOKEN_KIND_ERROR);
+                }
+            }
+        }
+
+        return t;
     }
 
-    char c;
-    cin >> c;
-
-    //cout << "c: " << c << endl;
-
-    if (cin) {
-        Token ct = Token(c);
-        //cout << ct.str() << endl;
-        return ct;
+    void push_back(Token t) {
+        m_next_token = t;
     }
 
-    return Token(TOKEN_KIND_ERROR);
-}
+private:
+    Token m_next_token;
+};
+
+TokenStream ts;
 
 double primary()
 {
-    Token t = get_token();
+    Token t = ts.get();
 
     return t.value;
 }
@@ -69,13 +92,13 @@ double term()
 {
     double left = primary();
 
-    Token t = get_token();
+    Token t = ts.get();
 
     while (true) {
         switch (t.kind) {
             case TOKEN_KIND_MUL:
                 left *= primary();
-                t = get_token();
+                t = ts.get();
                 break;
 
             case TOKEN_KIND_DIV:
@@ -87,11 +110,12 @@ double term()
                 }
 
                 left /= primary();
-                t = get_token();
+                t = ts.get();
                 break;
             }
 
             default:
+                ts.push_back(t);
                 return left;
         }
     }
@@ -101,22 +125,23 @@ double expression()
 {
     double left = term();
 
-    Token t = get_token();
+    Token t = ts.get();
 
     while (true) {
         switch (t.kind) {
             case TOKEN_KIND_PLUS:
                 left += term();
-                t = get_token();
+                t = ts.get();
                 break;
 
             case TOKEN_KIND_MINUS:
                 left -= term();
-                t = get_token();
+                t = ts.get();
                 break;
 
             default:
-                // error: t is not plus or minus but we've already stolen the next input
+                // bug: t is not plus or minus but we've already stolen the next input
+                ts.push_back(t);
                 return left; // term as expression
         }
     }
@@ -125,10 +150,22 @@ double expression()
 int main() {
 
     try {
-        //double result = expression();
+        double val = 0;
 
         while (cin) {
-            cout << "= " << expression() << endl;
+            Token t = ts.get();
+
+            if (t.kind == TOKEN_KIND_QUIT) {
+                break;
+            }
+
+            if (t.kind == TOKEN_KIND_EVAL) {
+                cout << "= " << val << endl;
+            } else {
+                ts.push_back(t);
+            }
+
+            val = expression();
         }
 
         return 0;
