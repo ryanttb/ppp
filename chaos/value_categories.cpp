9fe4e4a8356380f3d45d2cbde5fec504792421f1
ryanttb <ryan@trippingthebits.com>
2025-10-24 21:32:44 -0400

    simple pointer tests

--- chaos/value_categories.cpp
+++ chaos/value_categories.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <utility>
+#include <memory>
 
 using namespace std;
 
@@ -24,6 +25,28 @@ int main() {
 	
 	// xValue is now in a moved-from state (implementation defined)
 	// It's still accessible but its value is unspecified
+
+	// Smart pointer examples
+	cout << "\n--- Smart Pointer Examples ---" << endl;
+	
+	// unique_ptr example - exclusive ownership
+	unique_ptr<int> uniquePtr = make_unique<int>(100);
+	cout << "unique_ptr value: " << *uniquePtr << endl;
+	
+	// unique_ptr can be moved (transfers ownership)
+	unique_ptr<int> anotherUnique = move(uniquePtr);
+	cout << "After move - uniquePtr is null: " << (uniquePtr == nullptr) << endl;
+	cout << "anotherUnique value: " << *anotherUnique << endl;
+	
+	// shared_ptr example - shared ownership
+	shared_ptr<int> sharedPtr1 = make_shared<int>(200);
+	shared_ptr<int> sharedPtr2 = sharedPtr1; // shared ownership
+	cout << "shared_ptr1 use_count: " << sharedPtr1.use_count() << endl;
+	cout << "shared_ptr2 use_count: " << sharedPtr2.use_count() << endl;
+	cout << "Both point to same value: " << (*sharedPtr1 == *sharedPtr2) << endl;
+	
+	// When sharedPtr2 goes out of scope, use_count decreases
+	// When use_count reaches 0, the object is automatically deleted
 
     return 0;
 }
